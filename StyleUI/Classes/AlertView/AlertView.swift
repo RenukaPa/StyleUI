6bb3222c0977b1c214966122e5a210c4c56e3688
Renuka <renukapandey90@gmail.com>
2022-07-30 19:27:21 +0530

    added alert

---
+++ StyleUI/Classes/AlertView/AlertView.swift
@@ -0,0 +1,135 @@
+//
+//  AlertView.swift
+//  FBSnapshotTestCase
+//
+//  Created by Renuka Pandey on 29/07/22.
+//
+
+import Foundation
+import UIKit
+
+public class AlertView: UIView {
+    @IBOutlet weak var bgView: UIView!
+    @IBOutlet weak var lbl: UILabel!
+    
+    var dissmiss: (() -> Void)?
+    
+    public enum AlertType {
+        case warn
+        case success
+        case fail
+        case notify
+    }
+    
+    var type: AlertType = .success {
+        didSet {
+            var bgColor: UIColor!
+            switch type {
+            case .success:
+                bgColor = .green
+            case .fail:
+                bgColor = .red
+            case .warn:
+                bgColor = .orange
+            case .notify:
+                bgColor = .lightGray
+            }
+            self.bgView.backgroundColor = bgColor
+        }
+    }
+    
+    public var textContent: String? {
+        willSet(value) {
+            self.lbl.text = value
+        }
+    }
+    
+    convenience init() {
+        self.init(frame: CGRect.zero)
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        initializeContent()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        initializeContent()
+    }
+    
+    private func initializeContent() {
+        self.bgView.backgroundColor = UIColor.clear
+        _ = fromNib(nibName: String(describing: AlertView.self), isInherited: true)
+        let upSwipe = UISwipeGestureRecognizer(target: self, action: #selector(respondToSwipe(gesture:)))
+        upSwipe.direction = .up
+        upSwipe.delegate = self
+        bgView.isUserInteractionEnabled = true
+        bgView.addGestureRecognizer(upSwipe)
+    }
+}
+
+extension AlertView: UIGestureRecognizerDelegate {
+    @objc func respondToSwipe(gesture: UIGestureRecognizer) {
+        if let swipe = gesture as? UISwipeGestureRecognizer {
+            if swipe.direction == .up{
+                DispatchQueue.main.async {
+                    self.dissmiss?()
+                }
+            }
+        }
+    }
+}
+
+extension UIView {
+    func fromNib<T: UIView>(nibName: String, isInherited: Bool = false) -> T? {
+        let bundle = Bundle(for: type(of: self))
+        guard let customView = bundle.loadNibNamed(nibName, owner: self, options: nil)?.first as? T else {
+            return nil
+        }
+        customView.backgroundColor = .clear
+        if isInherited {
+            self.insertSubview(customView, at: 0)
+        } else {
+            self.addSubview(customView)
+        }
+        
+        customView.translatesAutoresizingMaskIntoConstraints = false
+        customView.viewConstraint(view: self)
+        return customView
+    }
+    
+    func viewConstraint(view: UIView) {
+        NSLayoutConstraint(item: self,
+                           attribute: .leading,
+                           relatedBy: .equal,
+                           toItem: view,
+                           attribute: .leading,
+                           multiplier: 1.0,
+                           constant: 0).isActive = true
+        
+        NSLayoutConstraint(item: self,
+                           attribute: .trailing,
+                           relatedBy: .equal,
+                           toItem: view,
+                           attribute: .trailing,
+                           multiplier: 1.0,
+                           constant: 0).isActive = true
+
+        NSLayoutConstraint(item: self,
+                           attribute: .top,
+                           relatedBy: .equal,
+                           toItem: view,
+                           attribute: .top,
+                           multiplier: 1.0,
+                           constant: 0).isActive = true
+
+        NSLayoutConstraint(item: self,
+                           attribute: .bottom,
+                           relatedBy: .equal,
+                           toItem: view,
+                           attribute: .bottom,
+                           multiplier: 1.0,
+                           constant: 0).isActive = true
+    }
+}
---
+++ StyleUI/Classes/AlertView/TriggerAlertView.swift
@@ -0,0 +1,85 @@
+//
+//  TriggerAlertView.swift
+//  Pods-StyleUI_Example
+//
+//  Created by Renuka Pandey on 30/07/22.
+//
+
+import Foundation
+import UIKit
+
+public class TriggerAlertView {
+    public init() {}
+    var alertView: AlertView!
+    let alertHeight: CGFloat = 100.0
+    var defaultYPos: CGFloat = -100.0
+    var animationTime: TimeInterval = 0.5
+    var timer: Timer?
+    var openAlertInBottom: (() -> Void)?
+    
+    public func setAlert(message: String? = "",
+                         type: AlertView.AlertType,
+                     fromBottom: (() -> Void)?) {
+        self.openAlertInBottom = fromBottom
+    }
+    
+    func showAlertView(msg: String) {
+        if self.alertView == nil {
+            let rect = CGRect(x: 0,
+                              y: defaultYPos,
+                              width: UIApplication.shared.keyWindow?.frame.width ?? 0.0,
+                              height: alertHeight)
+            alertView = AlertView(frame: rect)
+        }
+        
+        alertView.dissmiss = {
+            self.timer?.invalidate()
+            UIView.animate(withDuration: self.animationTime,
+                           delay: 0.0,
+                           options: [.allowUserInteraction],
+                           animations: {self.alertView.frame.origin.y = self.defaultYPos},
+                           completion: { _ in
+                            self.alertView.removeFromSuperview()
+                            self.alertView = nil
+                           })
+        }
+        
+        alertView.textContent = msg
+        if let window = UIApplication.shared.keyWindow {
+            window.addSubview(alertView)
+            window.bringSubviewToFront(alertView)
+        }
+        self.startAnimation()
+    }
+    
+    func startAnimation() {
+        guard alertView != nil else {
+            return
+        }
+        
+        UIView.animate(withDuration: animationTime) {
+            self.alertView.frame.origin.y = 0.0
+        }
+        
+        timer = Timer.scheduledTimer(timeInterval: 5.0,
+                                     target: self,
+                                     selector: #selector(hideAlertView),
+                                     userInfo: nil,
+                                     repeats: false)
+    }
+    
+    @objc func hideAlertView() {
+        if alertView != nil {
+            UIView.animate(withDuration: self.animationTime,
+                           delay: 5.0,
+                           options: [.allowUserInteraction],
+                           animations: {
+                            self.alertView.frame.origin.y = self.defaultYPos
+                           },
+                           completion: { _ in
+                            self.alertView.removeFromSuperview()
+                            self.alertView = nil
+                           })
+        }
+    }
+}
